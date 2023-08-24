454f75c485aa5fbee5e242c75d01da3753cf2200
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2023-08-24 14:13:17 -0400

    add section

---
+++ section.go
@@ -0,0 +1,32 @@
+package carbon
+
+import (
+	"io"
+)
+
+type section struct {
+	children any
+	attrs    []Attr
+}
+
+var _ Component = (*section)(nil)
+
+func Section(a ...any) *section {
+	return &section{
+		children: a,
+		attrs:    nil,
+	}
+}
+
+func (s *section) Attr(name string, value string) Component {
+	s.attrs = append(s.attrs, Attr{name, value})
+	return s
+}
+
+func (s *section) Render(w io.Writer) {
+	w.Write([]byte(`<section`))
+	renderAttrs(w, s.attrs)
+	w.Write([]byte(`>`))
+	renderAny(w, s.children)
+	w.Write([]byte(`</section>`))
+}
