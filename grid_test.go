77a551474d881850ac48f9eb00711deb9ebd1784
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2023-08-24 16:22:07 -0400

    fix test

--- grid_test.go
+++ grid_test.go
@@ -15,7 +15,7 @@ func TestGrid(t *testing.T) {
 }
 
 func TestGridColumnsRows(t *testing.T) {
-	expected := `<div class="bx--grid"><div class="bx--row"><div class="bx--col"><p>Hello World!</p></div></div><div class="bx--row"><div class="bx--col"><p>My name is Jason.</p></div></div></div>`
+	expected := `<div class="bx--grid"><div class="bx--col"><div class="bx--row"><p>Hello World!</p></div></div><div class="bx--col"><div class="bx--row"><p>My name is Jason.</p></div></div></div>`
 	// Test that the HTML output of a grid component is correct.
 	var buf bytes.Buffer
 	Grid(
